65cd1b2116f0ffb9a0e3c091fc9ce7c136e740c2
Donald Azevedo <dba9514@gmail.com>
2024-06-13 15:47:49 -0400

    Added the car rental form component

---
+++ src/app/car-rental-form/car-rental-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-rental-form',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      car-rental-form works!
+    </p>
+  `,
+  styleUrl: './car-rental-form.component.css'
+})
+export class CarRentalFormComponent {
+
+}
