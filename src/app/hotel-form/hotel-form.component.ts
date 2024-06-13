d00525b97eb6b8364538801799a59831ccd2e650
Donald Azevedo <dba9514@gmail.com>
2024-06-13 16:10:53 -0400

    Added the hotel form

---
+++ src/app/hotel-form/hotel-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-form',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      hotel-form works!
+    </p>
+  `,
+  styleUrl: './hotel-form.component.css'
+})
+export class HotelFormComponent {
+
+}
