1fa206a3be1d8406d928df850a63bddc8c683edb
Donald Azevedo <dba9514@gmail.com>
2024-06-13 14:08:49 -0400

    Built the general layout for the results

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -5,12 +5,24 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      results works!
-    </p>
+    <div>
+      <div class="card border-left-success shadow py-1 mb-5">
+        <div class="card-header py-2">
+          <h6 class="m-0 font-weight-bold text-success">Results</h6>
+        </div>
+        <div class="card-body">
+          <p>Markup Percentage: 20%</p>
+          <p>Additional Costs: $0.00</p>
+          <p>Flight 1 Cost: $100.00</p>
+          <p>Flight 2 Cost: $100.00</p>
+          <p>Car Rental Cost: $100.00</p>
+          <p>Hotel Cost: $100.00</p>
+          <p class="font-weight-bold">Total Cost: $480.00</p>
+          <button class="btn btn-success" type="button">Calculate total</button>
+        </div>
+      </div>
+    </div>
   `,
-  styleUrl: './results.component.css'
+  styleUrl: './results.component.css',
 })
-export class ResultsComponent {
-
-}
+export class ResultsComponent {}
