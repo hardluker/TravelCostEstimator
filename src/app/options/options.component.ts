024f4241bc15671e63c12b2cabf2404e0b74a160
Donald Azevedo <dba9514@gmail.com>
2024-06-13 10:32:35 -0400

    Adding bootstrap styling

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -5,23 +5,42 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <section class="options-container">
-      <form class="checkbox-form">
-        <label><input type="checkbox" name="flight1" />Flight 1</label><br />
-        <label><input type="checkbox" name="flight2" />Flight 2</label><br />
-        <label><input type="checkbox" name="carRental" />Car Rental</label
-        ><br />
-        <label><input type="checkbox" name="hotels" />Hotels</label>
-      </form>
-      <form class="number-inputs-form">
-        <div>
-          <label>Markup %<input type="number" /></label>
+    <section class="container-fluid">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card border-left-primary shadow h-100 w-100 py-1">
+            <form class="card-body">
+              <label><input type="checkbox" name="flight1" />Flight 1</label
+              ><br />
+              <label><input type="checkbox" name="flight2" />Flight 2</label
+              ><br />
+              <label><input type="checkbox" name="carRental" />Car Rental</label
+              ><br />
+              <label><input type="checkbox" name="hotels" />Hotels</label>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Additional Costs<input type="number" /></label>
+        <div class="col-md-3">
+          <div class="card border-left-primary shadow h-100 w-100 py-2">
+            <form class="user p-2">
+              <div class="form-group">
+                <input
+                  class="form-control form-control-user"
+                  type="number"
+                  placeholder="Markup %"
+                />
+              </div>
+              <div class="form-group pt-3">
+                <input
+                  class="form-control form-control-user"
+                  type="number"
+                  placeholder="Additional Costs"
+                />
+              </div>
+            </form>
+          </div>
         </div>
-        <button class="btn btn-circle" type="button">Press Me</button>
-      </form>
+      </div>
     </section>
   `,
   styleUrl: './options.component.css',
