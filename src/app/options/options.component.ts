a90031e7e30c40ab759269a193b11c7b72f3d93e
Donald Azevedo <dba9514@gmail.com>
2024-06-14 12:27:45 -0400

    The forms are now dependent on the checkboxes

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { FlightFormComponent } from '../flight-form/flight-form.component';
 import { ResultsComponent } from '../results/results.component';
 import { CarRentalFormComponent } from '../car-rental-form/car-rental-form.component';
 import { HotelFormComponent } from '../hotel-form/hotel-form.component';
+
 @Component({
   selector: 'app-options',
   standalone: true,
   imports: [
+    CommonModule,
+    FormsModule,
     FlightFormComponent,
     CarRentalFormComponent,
     HotelFormComponent,
@@ -21,18 +26,26 @@ import { HotelFormComponent } from '../hotel-form/hotel-form.component';
               <h6 class="m-0 font-weight-bold text-primary">Expenses</h6>
             </div>
             <form class="card-body">
-              <input type="checkbox" name="flight1" /><label class="pl-1"
-                >Flight 1</label
-              ><br />
-              <input type="checkbox" name="flight2" /><label class="pl-1"
-                >Flight 2</label
-              ><br />
-              <input type="checkbox" name="car-rental" /><label class="pl-1"
-                >Car Rental</label
-              ><br />
-              <input type="checkbox" name="hotel" /><label class="pl-1"
-                >Hotel</label
-              ><br />
+              <input
+                type="checkbox"
+                [(ngModel)]="showFlight1"
+                name="flight1"
+              /><label class="pl-1">Flight 1</label><br />
+              <input
+                type="checkbox"
+                [(ngModel)]="showFlight2"
+                name="flight2"
+              /><label class="pl-1">Flight 2</label><br />
+              <input
+                type="checkbox"
+                [(ngModel)]="showCarRental"
+                name="car-rental"
+              /><label class="pl-1">Car Rental</label><br />
+              <input
+                type="checkbox"
+                [(ngModel)]="showHotel"
+                name="hotel"
+              /><label class="pl-1">Hotel</label><br />
             </form>
           </div>
         </div>
@@ -65,16 +78,16 @@ import { HotelFormComponent } from '../hotel-form/hotel-form.component';
     </section>
     <section class="container-fluid mt-3">
       <div class="row">
-        <div class="col-lg-3 col-md-6 mb-3">
+        <div class="col-lg-3 col-md-6 mb-3" *ngIf="showFlight1">
           <app-flight-form></app-flight-form>
         </div>
-        <div class="col-lg-3 col-md-6 mb-3">
+        <div class="col-lg-3 col-md-6 mb-3" *ngIf="showFlight2">
           <app-flight-form></app-flight-form>
         </div>
-        <div class="col-lg-3 col-md-6 mb-3">
+        <div class="col-lg-3 col-md-6 mb-3" *ngIf="showCarRental">
           <app-car-rental-form></app-car-rental-form>
         </div>
-        <div class="col-lg-3 col-md-6 mb-3">
+        <div class="col-lg-3 col-md-6 mb-3" *ngIf="showHotel">
           <app-hotel-form></app-hotel-form>
         </div>
       </div>
@@ -83,6 +96,11 @@ import { HotelFormComponent } from '../hotel-form/hotel-form.component';
       <app-results></app-results>
     </section>
   `,
-  styleUrl: './options.component.css',
+  styleUrls: ['./options.component.css'],
 })
-export class OptionsComponent {}
+export class OptionsComponent {
+  showFlight1 = false;
+  showFlight2 = false;
+  showCarRental = false;
+  showHotel = false;
+}
