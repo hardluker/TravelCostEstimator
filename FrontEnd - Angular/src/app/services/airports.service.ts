27c380843664db352c99e24be400e29b13002c3b
hardluker <dba9514@gmail.com>
2024-06-15 15:34:42 -0400

    added airport service for getting all airports

---
+++ FrontEnd - Angular/src/app/services/airports.service.ts
@@ -0,0 +1,24 @@
+// src/app/services/airport.service.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Airport {
+  city: string;
+  state: string;
+  iata: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AirportsService {
+  private apiUrl = 'http://localhost:8000/api/airports/';
+
+  constructor(private http: HttpClient) {}
+
+  getAirports(): Observable<Airport[]> {
+    return this.http.get<Airport[]>(this.apiUrl);
+  }
+}
