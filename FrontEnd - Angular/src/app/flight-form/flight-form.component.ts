5b80bf223e801b7790951a6a378654ce7e3eccbe
hardluker <dba9514@gmail.com>
2024-06-15 17:45:33 -0400

    Moved the HTML template into a separate file

--- FrontEnd - Angular/src/app/flight-form/flight-form.component.ts
+++ FrontEnd - Angular/src/app/flight-form/flight-form.component.ts
@@ -1,7 +1,10 @@
-// src/app/flight-form/flight-form.component.ts
-
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { AirportsService } from '../services/airports.service';
 import { Airport } from '../services/airports.service';
@@ -12,89 +15,31 @@ import { finalize } from 'rxjs/operators';
 @Component({
   selector: 'app-flight-form',
   standalone: true,
-  imports: [FormsModule, NgSelectModule, CommonModule],
-  template: `
-    <div class="card border-left-primary shadow py-1">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Flight</h6>
-      </div>
-      <form class="card-body" (ngSubmit)="onSubmit()">
-        <div class="form-group">
-          <label for="departure-airport">Departure Airport</label>
-          <ng-select
-            id="departure-airport"
-            name="departureAirport"
-            [(ngModel)]="departureAirport"
-            [items]="airports"
-            required
-          >
-          </ng-select>
-        </div>
-        <div class="form-group">
-          <label for="arrival-airport">Arrival Airport</label>
-          <ng-select
-            id="arrival-airport"
-            name="arrivalAirport"
-            [(ngModel)]="arrivalAirport"
-            [items]="airports"
-            required
-          >
-          </ng-select>
-        </div>
-        <div class="form-group">
-          <label for="flight-date">Date</label>
-          <input
-            id="flight-date"
-            [(ngModel)]="flightDate"
-            name="flightDate"
-            class="form-control form-control-user"
-            type="date"
-            required
-          />
-        </div>
-        <div class="user form-group">
-          <label for="carrier">Carrier</label>
-          <ng-select name="carrier" [(ngModel)]="carrier" [items]="carriers">
-          </ng-select>
-        </div>
-        <div class="user form-group">
-          <button class="btn btn-primary" type="submit" [disabled]="searching">
-            Search
-          </button>
-        </div>
-      </form>
-      <div
-        *ngIf="searching"
-        class="spinner-border text-primary"
-        role="status"
-      ></div>
-      <div *ngIf="results" class="average-cost alert alert-primary">
-        Average Cost: {{ averageCost | currency }}
-      </div>
-      <div *ngIf="empty" class="average-cost alert alert-danger">
-        No results found
-      </div>
-    </div>
-  `,
+  imports: [ReactiveFormsModule, NgSelectModule, CommonModule],
+  templateUrl: './flight-form.component.html',
   styleUrls: ['./flight-form.component.css'],
 })
 export class FlightFormComponent implements OnInit {
-  departureAirport: string = '';
-  arrivalAirport: string = '';
-  flightDate: string = '';
-  carrier: string = '';
+  flightForm: FormGroup;
   averageCost: number = 0;
   searching: boolean = false;
   results: boolean = false;
-  airportMap: { [key: string]: string } = {};
   airports: string[] = [];
   carriers: string[] = ['All', 'American Airlines', 'Delta', 'United'];
   empty: boolean = false;
 
   constructor(
+    private fb: FormBuilder,
     private airportsService: AirportsService,
     private flightService: FlightService
-  ) {}
+  ) {
+    this.flightForm = this.fb.group({
+      departureAirport: ['', Validators.required],
+      arrivalAirport: ['', Validators.required],
+      flightDate: ['', Validators.required],
+      carrier: ['All'],
+    });
+  }
 
   ngOnInit() {
     this.airportsService.getAirports().subscribe(
@@ -110,9 +55,15 @@ export class FlightFormComponent implements OnInit {
   }
 
   onSubmit() {
-    const fromEntityId = this.getEntityId(this.departureAirport);
-    const toEntityId = this.getEntityId(this.arrivalAirport);
-    const departDate = this.flightDate;
+    if (this.flightForm.invalid) {
+      return;
+    }
+
+    const fromEntityId = this.getEntityId(
+      this.flightForm.value.departureAirport
+    );
+    const toEntityId = this.getEntityId(this.flightForm.value.arrivalAirport);
+    const departDate = this.flightForm.value.flightDate;
     this.searching = true;
     this.results = false;
     this.empty = false;
@@ -127,8 +78,6 @@ export class FlightFormComponent implements OnInit {
               .getFlightItineraries(fromEntityId, toEntityId, departDate)
               .pipe(
                 finalize(() => {
-                  // This block will execute when the second observable stream completes,
-                  // regardless of whether it completes successfully or with an error.
                   this.searching = false; // Set searching to false when search completes
                 })
               )
@@ -143,8 +92,8 @@ export class FlightFormComponent implements OnInit {
                       return itinerary.legs.some((leg: any) => {
                         return leg.carriers.marketing.some((carrier: any) => {
                           return (
-                            carrier.name === this.carrier ||
-                            this.carrier === 'All'
+                            carrier.name === this.flightForm.value.carrier ||
+                            this.flightForm.value.carrier === 'All'
                           );
                         });
                       });
@@ -189,9 +138,5 @@ export class FlightFormComponent implements OnInit {
     // Get the last part (which should be the airport code)
     const airportCode = parts[parts.length - 1].trim();
     return airportCode;
-  }
-
-  getKeys(map: any) {
-    return Object.keys(map);
   }
 }
