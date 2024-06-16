a5396e6a3d8963ac82bdf23b639a99ce37f10ac9
hardluker <dba9514@gmail.com>
2024-06-16 15:16:03 -0400

    Implementing logic for dynamically displaying and calculating costs in the results component

--- FrontEnd - Angular/src/app/results/results.component.ts
+++ FrontEnd - Angular/src/app/results/results.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type CostType = 'flight1' | 'flight2' | 'carRental' | 'hotel';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
     <div>
       <div class="card border-left-success shadow py-1 mb-5">
@@ -21,39 +24,75 @@ import { Component } from '@angular/core';
             <tbody>
               <tr>
                 <th scope="row">Markup Percentage</th>
-                <td>20%</td>
+                <td>{{ markupPercentage }}%</td>
               </tr>
               <tr>
                 <th scope="row">Additional Costs</th>
-                <td>$0.00</td>
+                <td>\${{ additionalCosts.toFixed(2) }}</td>
               </tr>
-              <tr>
-                <th scope="row">Flight 1</th>
-                <td>$100.00</td>
-              </tr>
-              <tr>
-                <th scope="row">Flight 2</th>
-                <td>$100.00</td>
-              </tr>
-              <tr>
-                <th scope="row">Car Rental</th>
-                <td>$100.00</td>
-              </tr>
-              <tr>
-                <th scope="row">Hotel</th>
-                <td>$100.00</td>
+              <tr *ngFor="let cost of costsArray">
+                <th scope="row">{{ cost.label }}</th>
+                <td>\${{ cost.value.toFixed(2) }}</td>
               </tr>
               <tr>
                 <th scope="row">Total</th>
-                <td class="font-weight-bold">$480.00</td>
+                <td class="font-weight-bold">\${{ totalCost.toFixed(2) }}</td>
               </tr>
             </tbody>
           </table>
-          <button class="btn btn-success" type="button">Calculate total</button>
+          <button
+            class="btn btn-success"
+            type="button"
+            (click)="calculateTotalCost()"
+          >
+            Calculate total
+          </button>
         </div>
       </div>
     </div>
   `,
-  styleUrl: './results.component.css',
+  styleUrls: ['./results.component.css'],
 })
-export class ResultsComponent {}
+export class ResultsComponent implements OnChanges {
+  @Input() costs: { [key in CostType]: number } = {
+    flight1: 0,
+    flight2: 0,
+    carRental: 0,
+    hotel: 0,
+  };
+  @Input() markupPercentage = 0;
+  @Input() additionalCosts = 0;
+
+  costsArray: { label: string; value: number }[] = [];
+  totalCost = 0;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      changes['costs'] ||
+      changes['markupPercentage'] ||
+      changes['additionalCosts']
+    ) {
+      this.calculateCostsArray();
+      this.calculateTotalCost();
+    }
+  }
+
+  calculateCostsArray() {
+    this.costsArray = [
+      { label: 'Flight 1', value: this.costs.flight1 },
+      { label: 'Flight 2', value: this.costs.flight2 },
+      { label: 'Car Rental', value: this.costs.carRental },
+      { label: 'Hotel', value: this.costs.hotel },
+    ].filter((cost) => cost.value > 0);
+  }
+
+  calculateTotalCost() {
+    this.calculateCostsArray(); // Ensure costsArray is updated
+    const baseCost = Object.values(this.costs).reduce(
+      (sum, cost) => sum + cost,
+      0
+    );
+    const markupAmount = (this.markupPercentage / 100) * baseCost;
+    this.totalCost = baseCost + markupAmount + this.additionalCosts;
+  }
+}
