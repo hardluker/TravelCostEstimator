3691d005a87d7e9af5188d2a6273d90565053718
hardluker <dba9514@gmail.com>
2024-06-16 08:39:37 -0400

    Built out the backend feature for getting all cities

--- Backend - Django/api/urls.py
+++ Backend - Django/api/urls.py
@@ -1,7 +1,8 @@
 # api/urls.py
 from django.urls import path
-from .views import airport_list
+from .views import airport_list, city_list
 
 urlpatterns = [
     path('airports/', airport_list, name='airport-list'),
+    path('cities/', city_list, name='city-list'),
 ]
--- Backend - Django/api/views.py
+++ Backend - Django/api/views.py
@@ -24,3 +24,25 @@ def airport_list(request):
         return Response({"error": str(e)}, status=500)
 
     return Response(airports)
+
+
+@api_view(['GET'])
+def city_list(request):
+    file_path = settings.BASE_DIR / 'assets' / 'uscities.csv'
+    cities = []
+
+    try:
+        with open(file_path, mode='r') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                cities.append({
+                    "city": row["city"],
+                    "state": row["state_name"],
+                    "county": row["county_name"]
+                })
+    except FileNotFoundError:
+        return Response({"error": "CSV file not found"}, status=404)
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
+
+    return Response(cities)
